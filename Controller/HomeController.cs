669f937ecda7cecdf7f622caf37bf1d9585f62b6
Furkan Calik <mpmcsv@gmail.com>
2021-08-03 21:31:54 +0300

    Jwt manager and login method at controller is implemented

---
+++ Controller/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Construction_Personal_Tracking_System.JwtTokenAuthentication;
+using Construction_Personal_Tracking_System.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Construction_Personal_Tracking_System.Controller {
+
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase {
+
+        public readonly IJwtTokenAuthenticationManager jwtTokenAuthenticationManager;
+
+        public HomeController(IJwtTokenAuthenticationManager jwtTokenAuthenticationManager) {
+            this.jwtTokenAuthenticationManager = jwtTokenAuthenticationManager;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Login([FromBody] UserInfo userInfo) {
+            string token = jwtTokenAuthenticationManager.Authenticate(userInfo.username, userInfo.password);
+            if(token == null) {
+                return Unauthorized();
+            }
+            string jsonToken = JsonConverter.
+            return JsonResult(token);
+        }
+    }
+}
---
+++ JwtTokenAuthentication/IJwtTokenAuthenticationManager.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Construction_Personal_Tracking_System.JwtTokenAuthentication {
+    public interface IJwtTokenAuthenticationManager {
+        string Authenticate(string username, string password);
+    }
+}
