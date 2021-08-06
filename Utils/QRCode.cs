d52c5b662a9f0dc770d34823c4a555b55aeec6b4
Furkan Calik <mpmcsv@gmail.com>
2021-08-06 17:49:11 +0300

    QRCode scanning method is implemented.

--- Utils/QRCode.cs
+++ Utils/QRCode.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 /// <author>Furkan Calik</author>
 namespace Construction_Personal_Tracking_System.Utils {
     public class QRCode {
+        public string Username { get; set; }
         public string CompanyName { get; set; }
         public string SectorName { get; set; }
         public double Latitude { get; set; }
